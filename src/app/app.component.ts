cf32705805dfb581ee31f876f7a76ea421823d64
rafael3994 <rafael3994@gmail.com>
2022-12-24 19:09:08 +0100

    prettier and esLint install

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Shopping-List';
+
+  test = [1, 2, 3, 4];
+}
