db91d8b8e97e4c88cb47666700fe94e463ed3bb4
Rafael3994 <rafael3994@gmail.com>
2023-08-12 11:22:42 +0200

    Products reals with Categories

--- src/app/modules/products/products.DTO.ts
+++ src/app/modules/products/products.DTO.ts
@@ -2,7 +2,7 @@ export interface ProductsDTO {
     isChecked: boolean,
     delete: string,
     name: String,
-    category: String,
+    category: Categories,
     brand: String, 
     price: Number, 
     formatSize: String,
@@ -10,3 +10,30 @@ export interface ProductsDTO {
     image: String,
     units?: Number,
 };
+
+export enum Categories {
+    HygieneandCleanliness = 'Hygiene and Cleanliness',
+    Costemica = 'Costemica',
+    Dairy = 'Dairy',
+    Alcohol = 'Alcohol',
+    Fruits = 'Fruits',
+    Vegetables = 'Vegetables',
+    MeatFishAndEggs = 'Meat, Fish and Eggs',
+    CerealsSugarAndSweets = 'Cereals, Sugar and Sweets',
+    FatsOilAndButter = 'Fats, oil and butter',
+    NaturalProducts = 'Natural products',
+}
+
+// Categorias
+// - Higiene y Limpieza
+// - Costemica
+// (Alimentos)
+//      - Láctios
+//      - Alcohol
+//      - Frutas
+//      - Verduras y Hortalizas
+//      - Carnes, pescados y huevos
+//      - Patatas, legumbres, frutos secos
+//      - Cereales y derivados, azúcar y dulces
+//      - Grasas, aceite y mantequilla
+//      - Productos naturales (agua)
