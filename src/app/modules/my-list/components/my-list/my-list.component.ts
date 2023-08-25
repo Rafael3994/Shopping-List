7d7ef9139e2719482974ddfc5599194b0db8d6fe
Rafael3994 <rafael3994@gmail.com>
2023-08-25 20:38:45 +0200

    clean list

--- src/app/modules/my-list/components/my-list/my-list.component.ts
+++ src/app/modules/my-list/components/my-list/my-list.component.ts
@@ -14,13 +14,14 @@ export class MyListComponent {
   displayedColumns: string[] = ['check', 'name', 'category', 'brand', 'price', 'units', 'delete'];
   dataSource: MatTableDataSource<ProductsDTO>;
   products: ProductsDTO[];
-  isDeleteSelect: boolean = false;
+  isShowModalDeleteProduct: boolean = false;
+  isShowModalCleanList: boolean = false;
   indexProduct: number | null = null;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild('empTbSort') empTbSort = new MatSort();
 
-  ngAfterViewInit() {    
+  ngAfterViewInit() {
     this.dataSource.sort = this.empTbSort;
   }
 
@@ -50,106 +51,148 @@ export class MyListComponent {
         marked: '',
         image: '',
       },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Arroz Basmati',
-        category: Categories.CerealsSugarAndSweets,
-        brand: 'Hacendado',
-        price: 2.00,
-        units: 3,
-        formatSize: '',
-        marked: 'Mercadona',
-        image: '',
-      },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Atún en Lata',
-        category: Categories.MeatFishAndEggs,
-        brand: 'Nixe',
-        price: 0.90,
-        units: 3,
-        formatSize: '',
-        marked: '',
-        image: '',
-      },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Yogur Natural',
-        category: Categories.Dairy,
-        brand: 'Hacendado',
-        price: 0.80,
-        units: 3,
-        formatSize: '',
-        marked: '',
-        image: '',
-      },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Spaghetti',
-        category: Categories.CerealsSugarAndSweets,
-        brand: 'Hacendado',
-        price: 1.00,
-        units: 3,
-        formatSize: '',
-        marked: '',
-        image: '',
-      },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Patatas Fritas',
-        category: Categories.Vegetables,
-        brand: 'Torres',
-        price: 1.80,
-        units: 1,
-        formatSize: '',
-        marked: 'Mercadona',
-        image: '',
-      },
-      {
-        isChecked: false,
-        delete: 'delete',
-        name: 'Agua Mineral',
-        category: Categories.NaturalProducts,
-        brand: 'Hacendado',
-        price: 3.15,
-        units: 3,
-        formatSize: '1.5L',
-        marked: 'Mercadona',
-        image: '',
-      },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Arroz Basmati',
+      //   category: Categories.CerealsSugarAndSweets,
+      //   brand: 'Hacendado',
+      //   price: 2.00,
+      //   units: 3,
+      //   formatSize: '',
+      //   marked: 'Mercadona',
+      //   image: '',
+      // },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Atún en Lata',
+      //   category: Categories.MeatFishAndEggs,
+      //   brand: 'Nixe',
+      //   price: 0.90,
+      //   units: 3,
+      //   formatSize: '',
+      //   marked: '',
+      //   image: '',
+      // },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Yogur Natural',
+      //   category: Categories.Dairy,
+      //   brand: 'Hacendado',
+      //   price: 0.80,
+      //   units: 3,
+      //   formatSize: '',
+      //   marked: '',
+      //   image: '',
+      // },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Spaghetti',
+      //   category: Categories.CerealsSugarAndSweets,
+      //   brand: 'Hacendado',
+      //   price: 1.00,
+      //   units: 3,
+      //   formatSize: '',
+      //   marked: '',
+      //   image: '',
+      // },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Patatas Fritas',
+      //   category: Categories.Vegetables,
+      //   brand: 'Torres',
+      //   price: 1.80,
+      //   units: 1,
+      //   formatSize: '',
+      //   marked: 'Mercadona',
+      //   image: '',
+      // },
+      // {
+      //   isChecked: false,
+      //   delete: 'delete',
+      //   name: 'Agua Mineral',
+      //   category: Categories.NaturalProducts,
+      //   brand: 'Hacendado',
+      //   price: 3.15,
+      //   units: 3,
+      //   formatSize: '1.5L',
+      //   marked: 'Mercadona',
+      //   image: '',
+      // },
     ]
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(this.products);
   }
 
-  showModal(e: Event, isChecked: boolean, index: number) {
+  // Show Delete Product Modal
+  showDeleleProductModal(e: Event, isChecked: boolean, index: number): void {
     e.stopPropagation();
-    if(isChecked) return;
+    if (isChecked) return;
     this.indexProduct = index;
     // document.getElementById('container')?.addEventListener('focus', (e) => e.preventDefault());
-    this.isDeleteSelect = !this.isDeleteSelect;
+    this.isShowModalDeleteProduct = !this.isShowModalDeleteProduct;
   }
 
-  deleteProduct() {
+  // Delete Product
+  deleteProduct(): void {
     if (this.indexProduct !== null) {
       this.products.splice(this.indexProduct, 1);
       this.dataSource = new MatTableDataSource(this.products);
-      this.closeModal();
+      this.closeDeleteProductModal();
     }
   }
 
-  closeModal() {
-    this.indexProduct = null;
-    this.isDeleteSelect = false;
+  // Check if all productes al check it
+  isListComplited(event: any, index: number) {
+    event.stopPropagation();
+    this.indexProduct = index;
+    const realCheckBox = !this.products[index].isChecked
+    // Are there some false?
+    const isCheckFalse = this.products.some((item: ProductsDTO, i: number) => {
+      if (index === i && !realCheckBox) {
+        return true;
+      }
+      if (index !== i && !item.isChecked) {
+        return true;
+      }
+      return
+    });
+
+    if(!isCheckFalse) {
+      this.isShowModalCleanList = true;
+    }
   }
 
-  applyFilter(event: Event) {
+  // Close delete product modal
+  closeDeleteProductModal(): void {
+    this.indexProduct = null;
+    this.isShowModalDeleteProduct = false;
+  }
+
+  // Close delete product modal
+  closeCleanListModal(): void {    
+    this.products[this.indexProduct!].isChecked = false;
+    this.indexProduct = null;
+    this.isShowModalCleanList = false;
+  }
+
+  cleanList(): void {
+    this.products = [];
+    this.dataSource = new MatTableDataSource(this.products);
+    this.isShowModalCleanList = false;
+  }
+
+  closeAllModals(): void {
+    this.closeDeleteProductModal(); 
+  }
+
+  // Filter products
+  applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
